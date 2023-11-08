7844d18041db94563920b7df63ed35aef5680d10
Сапожников Вячеслав <sapognikovslava@gmail.com>
2023-11-08 22:08:53 +0300

    Navigate to a new screen and back

---
+++ lib/features/navigator/first_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:lecture_6/features/navigator/second_screen.dart';
+
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Open route'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const SecondRoute(),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
